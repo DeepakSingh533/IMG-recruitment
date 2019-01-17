8adc3a701c53d087333f3d712451b9fc9b136446
DeepakSingh533 <45290842+DeepakSingh533@users.noreply.github.com>
2019-01-17 23:56:01 +0530

    Add files via upload

---
+++ shopping.js
@@ -0,0 +1,11 @@
+function SearchAlert(){
+    alert("We are Sorry. This is not a commercial online shopping site,It's just a website that Deepak has made to be recruited in the Team IMG.");
+}
+
+function SearchBoxColor(){
+	var newColor=document.getElementById("selectforsearch").value;
+	document.getElementById("searchbox").style.backgroundColor="newColor";
+}
+
+
+
